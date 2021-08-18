696ee4a92f02ec845a3989d3bc9c94fdee7e39cf
Denisse Nova <denisse.nova.v@gmail.com>
2021-08-17 20:08:37 -0400

    terminar classe reproductor

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,12 @@
+function video(url, id){
+  let idVideo = document.getElementById(id);
+  idVideo.setAttribute('src', url)
+}
+
+function videoPublica(url, id){
+  video(url, id)
+} 
+
 class Multimedia{
   constructor (_url){
     this.url= _url 
@@ -5,17 +14,27 @@ class Multimedia{
   get url(){
     return this.url
   }
-
-
+  setInicio (){
+    return "Este método es para realizar un cambio en la URL del video";
+};
 }
 
 class Reproductor extends Multimedia{
-  constructor(_url){
-
-  }
+  constructor(url, id){
+    super(url);
+    this._id = id;
+    this._getId = () =>this._id
+};
+playMultimedia (){
+    let id = this._getId();
+    let url = this._url;
+    sugerenciaVideo.mostrar(id, url);
+};
+setInicio (){
+    let id = this._getId();
+    let url = this._url;
+    url += `?start=${tiempo}`;
+    sugerenciaVideo.mostrar(id, url);
+};
 }
 
-function video(url, id){
-  let idVideo = document.getElementById(id);
-  idVideo.setAttribute('src', url)
-}
